03a12f5a74efdfc4770fdb232aff4285ea1f29a6
CLARA NARIELY DE MELO ALVES <20211170150067@ifrn.local>
2024-01-05 09:43:04 -0300

    som da moeda funcionando

--- durvalzito/Assets/Scripts/Coin.cs
+++ durvalzito/Assets/Scripts/Coin.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public int scoreValue;
+    private AudioSource sound;
 
+
+    private void Awake()
+    {
+        sound = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            sound.Play();
             GameController.instance.UpdateScore(scoreValue);
-            Destroy(gameObject);
+            Destroy(gameObject, 0.5f);
             
         }
     }
